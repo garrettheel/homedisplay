5dcba2724702e32c5a696997156ec9391da44f16
Garrett Heel <garrettheel@gmail.com>
2023-01-09 22:42:55 -0500

    split out display and data updates, leverage async

--- display.py
+++ display.py
@@ -1,4 +1,5 @@
-import time
+import asyncio
+import itertools
 import os
 from datetime import datetime
 from typing import Literal, Optional
@@ -19,10 +20,18 @@ except ImportError:
 A_TRAIN_IMAGE = Image.open('./img/a_train.png').convert('RGB')
 
 # Update every x seconds
-UPDATE_FREQ_SECONDS = 60
+DISPLAY_UPDATE_SECONDS = 30
+SUBWAY_UPDATE_SECONDS = 60
+WEATHER_UPDATE_SECONDS = 300
 
 ROWS = 32
 COLS = 64
+
+COLOUR_YELLOW = graphics.Color(255, 255, 0)
+COLOUR_LIGHT_GRAY = graphics.Color(211, 211, 211)
+COLOUR_LIGHT_BLUE = graphics.Color(204, 255, 255)
+COLOUR_LIGHT_ORANGE = graphics.Color(255, 229, 204)
+COLOUR_GREEN = graphics.Color(64, 255, 0)
 
 HOYT_SCHER_NORTH_STOP = "A42N"
 HOYT_SCHER_SOUTH_STOP = "A42S"
@@ -34,21 +43,121 @@ WeatherStatus = Literal["Clouds", "Rain", "Clear", "Snow"]
 Weather = tuple[int, int, int, int, Optional[WeatherStatus]]  # curr_temp, feels_like, min_temp, max_temp
 
 
-def run():
-    opts = RGBMatrixOptions()
-    opts.rows = ROWS
-    opts.cols = COLS
-    opts.hardware_mapping = 'adafruit-hat'
-    matrix = RGBMatrix(options=opts)
+class DisplayData:
+    def __init__(self):
+        self.subway_arrival_times: list[datetime] = []
+        self.weather: Optional[Weather] = None
 
+    def subway_arrival_deltas_minutes(self, num: int) -> list[str]:
+        now = now_eastern()
+
+        future_times = (t for t in self.subway_arrival_times if t >= now)
+        delta_mins = (int((t - now).total_seconds() // 60) for t in future_times)
+        return [str(t) for t in itertools.islice(delta_mins, num)]
+
+
+async def loop(matrix: RGBMatrix):
+    # Fire off tasks that will populate `data`
+    data = DisplayData()
+    asyncio.create_task(populate_subway_times(data))
+    asyncio.create_task(populate_weather(data))
+
+    # Give the initial data a good chance at being loaded on first render.
+    await asyncio.sleep(2)
+
+    # Render in the main loop
     while True:
-        arrival_times = subway_arrival_times()
-        weather = get_weather()
-        render(matrix, arrival_times, weather)
-        time.sleep(UPDATE_FREQ_SECONDS)
+        render(matrix, data)
+        await asyncio.sleep(DISPLAY_UPDATE_SECONDS)
 
 
-def subway_arrival_times() -> list[datetime]:
+def render(matrix: RGBMatrix, data: DisplayData) -> bool:
+    rendered_subway = rendered_weather = False
+    canvas = matrix.CreateFrameCanvas()
+
+    font = graphics.Font()
+    font.LoadFont('./fonts/6x10.bdf')
+
+    # Draw subway times
+    if data.subway_arrival_times:
+        arrival_minutes = data.subway_arrival_deltas_minutes(3)
+
+        times = ",".join(arrival_minutes)
+
+        # Draw graphic for the A train
+        image = Image.open('./img/a_train.png')
+        canvas.SetImage(image.convert('RGB'), offset_x=1, offset_y=1)
+
+        # Draw subway times
+        # TODO: different colours if the next train is more than 5 minutes away?
+        graphics.DrawText(canvas, font, 12, 8, COLOUR_YELLOW, times)
+
+        rendered_subway = True
+
+    # Draw weather
+    if data.weather:
+        curr_temp, _, min_temp, max_temp, weather_status = data.weather
+
+        curr_temp_str = f"{round(curr_temp)}°"
+        min_temp_str = f"{round(min_temp)}°"
+        max_temp_str = f"{round(max_temp)}°"
+
+        x_offset = 12  # room for weather image
+
+        graphics.DrawText(
+            canvas,
+            font,
+            x_offset,
+            18,
+            COLOUR_LIGHT_GRAY,
+            curr_temp_str,
+        )
+
+        # last number is padding between current temp and min/max
+        x_offset += (len(curr_temp_str) * 5) + 6
+
+        graphics.DrawText(
+            canvas,
+            font,
+            x_offset,
+            18,
+            COLOUR_LIGHT_BLUE,
+            min_temp_str,
+        )
+
+        x_offset += (len(min_temp_str) * 5) + 3
+
+        graphics.DrawText(
+            canvas,
+            font,
+            x_offset,
+            18,
+            COLOUR_LIGHT_ORANGE,
+            max_temp_str,
+        )
+
+        if weather_status:
+            try:
+                weather_image = Image.open(f'./img/{weather_status.lower()}.png')
+                canvas.SetImage(weather_image.convert('RGB'), offset_x=1, offset_y=11)
+            except FileNotFoundError:
+                print('No weather icon exists for', weather_status)
+
+        rendered_weather = True
+
+    matrix.SwapOnVSync(canvas)
+
+    # Return a bool to indicate whether we rendered everything expected
+    return rendered_subway and rendered_weather
+
+
+async def populate_subway_times(data: DisplayData) -> None:
+    while True:
+        data.subway_arrival_times = await subway_arrival_times()
+        await asyncio.sleep(SUBWAY_UPDATE_SECONDS)
+
+
+async def subway_arrival_times() -> list[datetime]:
     feed = get_subway_times()
     trip_updates = [e for e in feed.entity if e.HasField('trip_update')]
 
@@ -74,52 +183,12 @@ def subway_arrival_times() -> list[datetime]:
 
     north_arrival_times.sort()
 
+    return north_arrival_times
+
+
+def now_eastern() -> datetime:
     # MTA times do not have microsecond precision so just ignore that
-    now = datetime.utcnow().replace(microsecond=0, tzinfo=pytz.utc).astimezone(EASTERN_TZ)
-
-    seconds_till_north_arrival = []
-    for arrival in north_arrival_times:
-        # Don't add times in the past
-        if arrival > now:
-            seconds_till_north_arrival.append(arrival - now)
-    return seconds_till_north_arrival
-
-
-def render(matrix: RGBMatrix, north_arrival_deltas: list[datetime], weather: Weather):
-    canvas = matrix.CreateFrameCanvas()
-    canvas.Clear()
-
-    yellow = graphics.Color(255, 255, 0)
-    green = graphics.Color(64, 255, 0)
-
-    font = graphics.Font()
-    font.LoadFont('./fonts/6x10.bdf')
-
-    arrival_minutes = [str(t.seconds // 60) for t in north_arrival_deltas[:3]]
-
-    times = ",".join(arrival_minutes)
-
-    # Draw graphic for the A train
-    image = Image.open('./img/a_train.png')
-    canvas.SetImage(image.convert('RGB'), offset_x=1, offset_y=1)
-
-    # Draw subway times
-    # TODO: different colours if the next train is more than 5 minutes away?
-    graphics.DrawText(canvas, font, 12, 8, yellow, times)
-
-    # Draw weather
-    # TODO: different colours if it's cold/hot/raining/etc.
-    curr_temp, _, min_temp, max_temp, weather_status = weather
-    graphics.DrawText(canvas, font, 12, 18, green, f"{round(curr_temp)}° {round(min_temp)}-{round(max_temp)}")
-    if weather_status:
-        try:
-            weather_status = "Clouds"
-            weather_image = Image.open(f'./img/{weather_status.lower()}.png')
-            canvas.SetImage(weather_image.convert('RGB'), offset_x=1, offset_y=11)
-        except FileNotFoundError:
-            print('No weather icon exists for', weather_status)
-
-    matrix.SwapOnVSync(canvas)
+    return datetime.utcnow().replace(microsecond=0, tzinfo=pytz.utc).astimezone(EASTERN_TZ)
 
 
 def get_subway_times() -> gtfs_realtime_pb2.FeedMessage:
@@ -139,7 +208,13 @@ def get_subway_times() -> gtfs_realtime_pb2.FeedMessage:
     return feed
 
 
-def get_weather() -> Weather:
+async def populate_weather(data: DisplayData) -> None:
+    while True:
+        data.weather = await get_weather()
+        await asyncio.sleep(WEATHER_UPDATE_SECONDS)
+
+
+async def get_weather() -> Weather:
     api_key = os.getenv('OPENWEATHER_API_KEY')
     assert api_key, "OPENWEATHER_API_KEY not set"
 
@@ -167,4 +242,15 @@ def get_weather() -> Weather:
 
 
 if __name__ == '__main__':
-    run()
+    opts = RGBMatrixOptions()
+    opts.rows = ROWS
+    opts.cols = COLS
+    opts.hardware_mapping = 'adafruit-hat'
+    matrix = RGBMatrix(options=opts)
+
+    try:
+        asyncio.run(loop(matrix))
+    except KeyboardInterrupt:
+        pass
+    # Ensure screen is cleared on shutdown
+    matrix.Clear()
